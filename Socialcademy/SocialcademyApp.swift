05211db53f6c3dd3d4d1bacd57da13c525771484
TestName <48367659+SufferedComet@users.noreply.github.com>
2023-06-28 21:17:42 -0700

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Angelo Delgado on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
