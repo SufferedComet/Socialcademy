a0dfc522e83edb0bfb312d6bdc559961591f2f37
TestName <48367659+SufferedComet@users.noreply.github.com>
2023-06-30 18:59:49 -0700

    Update includes delete function. The function will delete data from the database. Animation added for the delete function.

--- Socialcademy/Views/PostRow.swift
+++ Socialcademy/Views/PostRow.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct PostRow: View {
+    typealias DeleteAction = () async throws -> Void
+    
     let post: Post
+    let deleteAction: DeleteAction
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
@@ -25,13 +28,27 @@ struct PostRow: View {
                 .font(.title3)
                 .fontWeight(.semibold)
             Text(post.content)
+            HStack {
+                Spacer()
+                Button(role: .destructive, action: deletePost) {
+                    Label("Delete", systemImage: "trash")
+                }
+                .labelStyle(.iconOnly)
+                .buttonStyle(.borderless)
+            }
         }
         .padding(.vertical)
+    }
+    
+    private func deletePost() {
+        Task {
+            try! await deleteAction()
+        }
     }
 }
 
 struct PostRow_Previews: PreviewProvider {
     static var previews: some View {
-        PostRow(post: Post.testPost)
+        PostRow(post: Post.testPost, deleteAction: {})
     }
 }
