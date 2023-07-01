69b8734e1f7676f0907e41e92509b607d504937d
TestName <48367659+SufferedComet@users.noreply.github.com>
2023-07-01 10:27:36 -0700

    PostRow reorganized. PostRowViewModel added.

--- Socialcademy/ViewModels/PostsViewModel.swift
+++ Socialcademy/ViewModels/PostsViewModel.swift
@@ -17,13 +17,6 @@ class PostsViewModel: ObservableObject {
         self.postsRepository = postsRepository
     }
     
-    func makeCreateAction() -> NewPostForm.CreateAction {
-        return { [weak self] post in
-            try await self?.postsRepository.create(post)
-            self?.posts.value?.insert(post, at: 0)
-        }
-    }
-    
     func fetchPosts() {
         Task {
             do {
@@ -35,10 +28,25 @@ class PostsViewModel: ObservableObject {
         }
     }
     
-    func makeDeleteAction(for post: Post) -> PostRow.DeleteAction {
-        return { [weak self] in
-            try await self?.postsRepository.delete(post)
-            self?.posts.value?.removeAll { $0.id == post.id }
+    func makeCreateAction() -> NewPostForm.CreateAction {
+        return { [weak self] post in
+            try await self?.postsRepository.create(post)
+            self?.posts.value?.insert(post, at: 0)
         }
     }
+    
+    func makePostRowViewModel(for post: Post) -> PostRowViewModel {
+        return PostRowViewModel(
+            post: post,
+            deleteAction: { [weak self] in
+            try await self?.postsRepository.delete(post)
+            self?.posts.value?.removeAll { $0.id == post.id }
+        }, favoriteAction: { [weak self] in
+            let newValue = !post.isFavorite
+            try await newValue ? self?.postsRepository.favorite(post) : self?.postsRepository.unfavorite(post)
+            guard let i = self?.posts.value?.firstIndex(of: post) else { return }
+            self?.posts.value?[i].isFavorite = newValue
+        })
+    }
+    
 }
